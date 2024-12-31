2d2d262d48d756e2b58053f7b1524dd9a292dad5
Nelson Chen <crazysim@gmail.com>
2024-12-30 19:49:39 -0800

    hello

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+
+// Create a Google Cloud resource (Storage Bucket)
+const bucket = new gcp.storage.Bucket("my-bucket", {
+  location: "US",
+});
+
+// Export the DNS name of the bucket
+export const bucketName = bucket.url;

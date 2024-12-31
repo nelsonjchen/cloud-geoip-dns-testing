febe3df5a76bffafe51bc7078c81c9f64040fedd
Nelson Chen <crazysim@gmail.com>
2024-12-30 20:13:35 -0800

    working test

--- index.ts
+++ index.ts
@@ -1,11 +1,34 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
+export = async () => {
+  const available = await gcp.compute.getRegions({});
+  const regionNames = available.names;
 
-// Create a Google Cloud resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-  location: "US",
-});
+  // Create a Google Cloud DNS Zone
+  const dnsZone = new gcp.dns.ManagedZone("test", {
+    dnsName: "gcp.geoip-test.mindflakes.com.",
+    description: "GCP zone for testing the GeoIP lookup",
+  });
 
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+  // Create a Google Cloud DNS Record Set
+  // We want to test with CNAME records, so we need to create a CNAME record for
+  // each region
+  // Just set examples
+
+  const recordSet = new gcp.dns.RecordSet("test", {
+    managedZone: dnsZone.name,
+    name: "test.gcp.geoip-test.mindflakes.com.",
+    type: "CNAME",
+    ttl: 60,
+    routingPolicy: {
+      geos: regionNames.map((region) => {
+        return {
+          location: region,
+          rrdatas: [`test-result-${region}.gcp.geoip-test.mindflakes.com.`],
+        };
+      })
+    },
+    project: "geoip-testing-446019",
+  });
+}
